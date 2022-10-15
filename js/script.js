8b5d2544a82b5758e8a7e377786fe24603616771
Alina  Lubiana <alina.furman28@gmail.com>
2022-10-15 21:11:34 +0300

    add results

--- js/script.js
+++ js/script.js
@@ -31,3 +31,43 @@ if(playerInput == '2'){
     playerMove = 'nożyce';
 }
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+}
+if( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Ty przegrawasz!');
+}
+if( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Mamy ramis!');
+}
+
+if( computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Ty przegrawasz!'); 
+}
+if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+}
+if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Mamy ramis!'); 
+}
+
+if( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Mamy ramis!'); 
+}
+if( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Ty przegrawasz!'); 
+}
+if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!'); 
+}
+
+if( computerMove == 'papier' && playerMove == 'nieznany ruch'){
+    printMessage('nieznany ruch'); 
+}
+if( computerMove == 'nożyce' && playerMove == 'nieznany ruch'){
+    printMessage('nieznany ruch'); 
+}
+if( computerMove == 'kamień' && playerMove == 'nieznany ruch'){
+    printMessage('nieznany ruch'); 
+}
