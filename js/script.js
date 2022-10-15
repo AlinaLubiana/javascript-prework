5d3b84b98cae5625edcce6069e24c7298e22a9ba
Alina  Lubiana <alina.furman28@gmail.com>
2022-10-15 18:13:43 +0300

    js1-1

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
 printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
 
 let computerMove = 'kamień'; 
-let playerMove = 'papier';
+let playerMove = 'papier1';
 
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 
